83815fbaeb63c6c2fdcd51ddaee33241d287ff25
Arthur <agw.174@gmail.com>
2017-11-07 15:54:36 +0500

    add send email

--- apps/reservation/views.py
+++ apps/reservation/views.py
@@ -11,33 +11,33 @@ class ReservationAPIView(CreateAPIView):
     serializer_class = ReservationSerializer
     authentication_classes = (UnsafeSessionAuthentication,)
 
-    # def perform_create(self, serializer):
-    #     instance = serializer.save()
-    #
-    #     created_at = instance.created_at.strftime("%Y-%m-%d %H:%M:%S")
-    #     title = "Новая бронь: {0}".format(created_at)
-    #     body = """
-    #     Имя: {name};
-    #     Телефон: {phone};
-    #     Дата: {date};
-    #     Время с: {time_start};
-    #     Время до: {time_end};
-    #     Количество человек: {people};
-    #     Комментарий: {comment}
-    #     """.format(
-    #         name=instance.name,
-    #         phone=instance.phone_number,
-    #         date=instance.date,
-    #         time_start=instance.time_start,
-    #         time_end=instance.time_end,
-    #         people=instance.count_people,
-    #         comment=instance.comment,
-    #     )
-    #
-    #     send_mail(
-    #         title,
-    #         body,
-    #         settings.EMAIL_HOST_USER,
-    #         ['reservation@borisbar.ru'],
-    #         fail_silently=False
-    #     )
+    def perform_create(self, serializer):
+        instance = serializer.save()
+
+        created_at = instance.created_at.strftime("%Y-%m-%d %H:%M:%S")
+        title = "Новая бронь: {0}".format(created_at)
+        body = """
+        Имя: {name};
+        Телефон: {phone};
+        Дата: {date};
+        Время с: {time_start};
+        Время до: {time_end};
+        Количество человек: {people};
+        Комментарий: {comment}
+        """.format(
+            name=instance.name,
+            phone=instance.phone_number,
+            date=instance.date,
+            time_start=instance.time_start,
+            time_end=instance.time_end,
+            people=instance.count_people,
+            comment=instance.comment,
+        )
+
+        send_mail(
+            title,
+            body,
+            settings.EMAIL_HOST_USER,
+            ['reservation@greenerybar.ru'],
+            fail_silently=False
+        )
