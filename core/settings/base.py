d09e1f59c248c3f32ba2e856c415f46c8b6fb313
Arthur <pyeye.91@gmail.com>
2019-08-26 21:08:34 +0500

    switch string to int port settings

--- core/settings/base.py
+++ core/settings/base.py
@@ -81,7 +81,7 @@ DATABASES = {
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'PORT': int(os.getenv("DB_PORT")),
     }
 }
 
